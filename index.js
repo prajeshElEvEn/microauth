893bdd45ce513dfedf7f3d3120d3e15eef36801b
Prajesh Pratap Singh <prajesh.eleven118@gmail.com>
2024-02-13 22:35:00 +0530

    added docs

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 app.use("/api/v1/health", health);
 app.use("/api/v1/auth", auth);
@@ -24,4 +25,5 @@ app.listen(port, hostname, async () => {
   warn(`Environment > ${currentEnv}`);
   await connectToDB(process.env.MONGO_URI);
   log(`Server running > http://${hostname}:${port}/api/v1/health`);
+  log(`View docs > http://${hostname}:${port}/docs`);
 });
